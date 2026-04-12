3cd2e2fb6663518c0caa2717f0009ea26c698580
Aminpatra <aminpatranew@gmail.com>
2026-04-12 09:38:13 +0400

    Create first version of faltten data function

--- NumMy.py
+++ NumMy.py
@@ -23,18 +23,19 @@ class NummyArray:
   def __valid_input(self, iterable):
     return isinstance(iterable, list) or isinstance(iterable, tuple)
   
-  def __flatten(self, iterable):
-    data = []
 
-
-    # if iterable:
-    #   if self.__valid_input(iterable) and not self.__valid_input(iterable[0]):
-    #     for element in iterable:
-    #       data.append(element)
-    #   return self.__flatten(iterable[0])
+  def __flatten(self, iterable, data = None):
     
-    # else:
-    #   return data
+    if data is None:
+      data = []
+    
+    if iterable:
+      for element in iterable:
+        if not self.__valid_input(element):
+          data.append(element)
+        else: 
+          self.__flatten(element, data)
+    return data
 
   def arr_shape(self, iterable, shape = None):
 
@@ -74,60 +75,7 @@ class NummyArray:
     return self.arr_type(iterable[0])
   
 
-a = NummyArray([[1, 2], [3, 0.1]])
+a = NummyArray([1,2])
 print(a.data)
   
   
-# d__1 = "string"
-# d0 = (1, 1)
-# d1 = []
-# d2 = [[1.0,2.2],
-#       [3.4,4.4]]
-# d3 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
-# d4 = [[[[[1,2,3]]]]]
-# d_not = [[1, 2], [3]]
-
-# print('NEW')
-# s = NummyArray(d__1)
-# arr1 = NummyArray(d0)
-# arr2 = NummyArray(d1)
-# arr3 = NummyArray(d2)
-# arr4 = NummyArray(d3)
-# arr5 = NummyArray(d4)
-# arr6 = NummyArray(d_not)
-
-# print(arr1.type)
-
-# print(arr1.size)
-# print(arr2.shape)
-# print(arr3.shape)
-# print(arr4.shape)
-# print(arr5.shape)
-# print(arr6.shape)
-# print(arr1.data)
-# print(arr2.data)
-# print(arr3.data)
-# print(arr4.data)
-# print(arr5.data)
-# print(arr6.data)
-
-# print(arr1.size)
-# print(arr1.shape)
-# print(arr2.size)
-# print(arr3.size)
-# print(arr4.shape)
-# print(arr4.data)
-# print(arr4.ndim)
-# print(arr4.size)
-# print(arr4.size)
-# print(arr4.type)
-# print(arr5.size)
-# print(arr6.size)
-
-
-# a = NummyArray([[1, 2], [3, 4]])
-# print(a.data)
-
-
-# Limitations of this library
-# 1- can't accept [[1, 2], [1]], unbalanced arrays
